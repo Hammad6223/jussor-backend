9de50d511007487d3ff0cbaa1d5e45bac512ea78
Hammad6223 <hammadakram6223@gmail.com>
2024-07-23 23:00:23 +0500

    cxccxc

--- connection/connection.js
+++ connection/connection.js
@@ -2,9 +2,9 @@ const mongoose = require("mongoose");
 module.exports = {
   connect: (cb) => {
     // const devUrl = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@localhost:27017/findlarry`;
-    const devUrl = `mongodb+srv://hammadakram6223:jgV3jy9qypzezaO1@cluster0.ungqcbm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+    // const devUrl = `mongodb+srv://hammadakram6223:jgV3jy9qypzezaO1@cluster0.ungqcbm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
     // const prodUrl = "";
-    mongoose.connect(devUrl, {
+    mongoose.connect(process.env.BASE_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
